f7a09bbbb36c6fc1e20c6628c2d94cd34822b2e8
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-10 15:18:19 -0800

    create-tables, drop-tables, and load-seed-data are all created

--- server/scripts/drop-tables.js
+++ server/scripts/drop-tables.js
@@ -1,1 +1,13 @@
-require('dotenv').config();
+const client = require('../lib/db-client');
+
+client.query(`
+    DROP TABLE IF EXISTS goal;
+    DROP TABLE IF EXISTS profile;
+`)
+    .then(
+        () => console.log('drop tables are done'),
+        err => console.log(err)
+    )
+    .then(() => {
+        client.end();
+    });
