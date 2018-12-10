70b937a179289737270ff82f133e0d5e884421de
Aaron Dennis <AMDennis1987@gmail.com>
2018-12-10 15:55:57 -0800

    app and auth.vue

--- app/src/components/home/Home.vue
+++ app/src/components/home/Home.vue
@@ -1,32 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <section>
+    <h2>Welcome to My Goals!</h2>
+    <p>
+      <RouterLink to="/goals">view goals</RouterLink>
+    </p>
+  </section>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+<style>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
