c9ad08c8de57c4a6e464bb18a35f77628d2b6905
PDXCrow <piratehans@gmail.com>
2018-12-10 15:04:06 -0800

    auth.js setup, signup working on backend

--- app/src/components/App.vue
+++ app/src/components/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <span v-if="user">
+        Hello {{user.username}}!
+      </span>
+      <nav v-if="user">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/pets">Goals</RouterLink>
+      </nav>
+    </header>
+
+    <main>
+      <RouterView v-if="user" :user="user"/>
+      <Auth v-else
+        :onSignUp="handleSignUp"
+        :onSignIn="handleSignIn"
+      />
+    </main>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      user: null
+    };
   }
-}
+  
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+
+header {
+  height: 75px;
+  background: lightsteelblue;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
+
+header img {
+  height: 100%;
+}
+
+nav a {
+  text-decoration: none;
+  color: black;
+  margin: 3px;
+  padding: 3px;
+  border: 1px solid black;
+}
+
+main {
+  padding: 8px;
+}
+
+
 </style>
