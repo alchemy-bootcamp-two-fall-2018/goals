64b4b5ad7fb1a6fea2e9d197e72492d046021d8e
Abel Quintero <abel.j.quintero@gmail.com>
2018-12-10 15:46:08 -0800

    AddGoal and Goal wired up

---
+++ app/src/components/goals/AddGoal.vue
@@ -0,0 +1,55 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Goal:</label>
+    <input v-focus v-model="goal.name" require>
+    <label>Category (i.e. health):</label>
+    <input v-focus v-model="goal.type" require>
+    <button>Add</button>
+  </form>
+</template>
+
+<script>
+
+function initGoal() {
+  return {
+    name: '',
+    type: ''
+  };
+}
+
+export default {
+  props: {
+    onAdd: initGoal()
+  
+  },
+  data() {
+    return {
+      goal: initGoal()
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.onAdd(this.goal)
+        .then(() => {
+          this.goal = initGoal();
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+form {
+  border: 1px solid #aaa;
+  padding: 10px;
+}
+input, button {
+  margin: 0 5px;
+}
+input {
+  width: 125px;
+  font-size: 1.05em;
+}
+
+</style>
---
+++ app/src/components/goals/Goal.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>{{goal.name}} under the category of {{goal.type}}</li>
+</template>
+
+<script>
+export default {
+  props: {
+    goal: null
+  }
+};
+</script>
+
+<style scoped>
+  li{
+    padding: 10px 0;
+  }
+</style>
