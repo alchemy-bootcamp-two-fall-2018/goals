a2d3d74c8ba26fc05e0ccbc9d4fae02985cc9816
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-12-10 18:11:07 -0800

    can POST user to postman

--- server/library/app.js
+++ server/library/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const auth = require('./routes/auth');
 
-app.use(morgan ('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/api/auth', auth);
+// app.use('/api/goals', goals);
 
 module.exports = app;
