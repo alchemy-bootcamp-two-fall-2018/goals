69a8d5334362ddaa0c2459be262b650d5b6c11a9
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-10 17:01:04 -0800

    can add new goals to a person who is signed in

--- server/lib/routes/goals.js
+++ server/lib/routes/goals.js
@@ -5,13 +5,31 @@ const router = Router();
 
 router.get('/', (req, res) => {
   client.query(`
-    SELECT id, title, start_date, end_date
+    SELECT 
+      id,
+      title, 
+      start_date as "startDate", 
+      end_date as "endDate"
     FROM goal
     WHERE profile_id = $1;
   `,
   [req.userId])
     .then(result => {
       res.json(result.rows);
+    });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+
+  client.query(`
+    INSERT INTO goal (title, start_date, end_date, profile_id)
+    VALUES($1, $2, $3, $4)
+    RETURNING *;
+  `,
+  [body.title, body.startDate, body.endDate, req.userId])
+    .then(result => {
+      res.json(result.rows[0]);
     });
 });
 
