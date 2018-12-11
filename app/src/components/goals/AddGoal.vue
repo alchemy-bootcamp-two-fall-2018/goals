45671e1ff7f2179e1e8fd0cf7a6c889cad84053e
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-10 17:17:30 -0800

    addgoal is completed

---
+++ app/src/components/goals/AddGoal.vue
@@ -0,0 +1,52 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <label>Name:</label>
+        <input v-focus v-model="pet.name" require>
+        <label>Type:</label>
+        <input v-focus v-model="pet.type" require>
+        <button>Add</button>
+  </form>
+</template>
+
+<script>
+
+function initGoal() {
+  return {
+    name: '',
+    type: ''
+  };
+}
+
+export default {
+  props: {
+    onAdd: Function
+  },
+  data() {
+    return {
+      goal: initGoal()
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.onAdd(this.goal)
+        .then(() => {
+          this.goal = initGoal();
+        });
+    }
+  }
+};
+</script>
+
+<style>
+form {
+  border: 1px solid #aaa;
+  padding: 10px;
+}
+input, button {
+  margin: 0 5px;
+}
+input {
+  width: 125px;
+  font-size: 1.05em;
+}
+</style>
