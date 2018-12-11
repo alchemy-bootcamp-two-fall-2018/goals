1a9a88f318fc45f935dd600bb58909fad7e89653
Heather <unphurlng@gmail.com>
2018-12-11 07:02:59 -0800

    username&password post on 1st POST, return already exists 2nd POST

--- server/lib/app.js
+++ server/lib/app.js
@@ -10,18 +10,18 @@ app.use(morgan('dev'));
 // register the json "middleware" body parser
 app.use(express.json());
 
-function checkAuth(req, res, next) {
-  const userId = req.get('Authorization');
-  if(!userId) {
-    res.status(401).json({ error: 'no authorization found' });
-    return;
-  }
-  req.userId = userId;
-  next();
-}
+// function checkAuth(req, res, next) {
+//   const userId = req.get('Authorization');
+//   if(!userId) {
+//     res.status(401).json({ error: 'no authorization found' });
+//     return;
+//   }
+//   req.userId = userId;
+//   next();
+// }
 
 // register our routes
 app.use('/api/auth', auth);
-app.use('/api/goals', checkAuth, goals);
+// app.use('/api/goals', checkAuth, goals);
 
 module.exports = app;
