3ad4f0c24f42abd1050329d8ba1db222a3fb7e42
Paige Gorry <paigeegorry@gmail.com>
2018-12-11 07:16:58 -0800

    able to sign in via postman

--- server/lib/routes/auth.js
+++ server/lib/routes/auth.js
@@ -3,34 +3,61 @@ const client = require('../db-client');
 const Router = express.Router;
 const router = Router(); //eslint-disable-line new-cap
 
-router.post('/signup', (req, res) => {
-  const body = req.body;
-  const username = body.username;
-  const password = body.password;
+router
+  .post('/signup', (req, res) => {
+    const body = req.body;
+    const username = body.username;
+    const password = body.password;
 
-  if(!username || !password){
-    res.status(400).send({ error: 'username and password required' });
-    return;
-  }
-  client.query(`
+    if(!username || !password){
+      res.status(400).send({ error: 'username and password required' });
+      return;
+    }
+    client.query(`
     SELECT id 
     FROM profile
     WHERE username = $1
-  `, [username])
-    .then(result => {
-      if(result.rows.length > 0){
-        res.status(400).send({ error: 'username already exists' });
+    `, [username])
+      .then(result => {
+        if(result.rows.length > 0){
+          res.status(400).send({ error: 'username already exists' });
+          return;
+        }
+        client.query(`
+            INSERT INTO profile(username, password)
+            VALUES ($1, $2)
+            RETURNING id, username;
+        `, [username, password])
+          .then(result => {
+            res.json(result.rows[0]);
+          });
+      });
+  })
+  .post('/signin', (req, res) => {
+    const body = req.body;
+    const username = body.username;
+    const password = body.password;
+
+    if(!username || !password){
+      res.status(400).send({ error: 'username and password required' });
+      return;
+    }
+
+    client.query(`
+        SELECT id, username, password
+        FROM profile
+        WHERE username = $1
+    `, [username]).then(result => {
+      if(result.rows.length === 0 || result.rows[0].password !== password){
+        res.status(400).send({ error: 'username and password required' });
         return;
       }
-      client.query(`
-        INSERT INTO profile(username, password)
-        VALUES ($1, $2)
-        RETURNING *;
-      `, [username, password])
-        .then(result => {
-          res.json(result.rows[0]);
-        });
+
+      res.json({
+          id: result.rows[0].id,
+          username: result.rows[0].username,
+      });
     });
-});
+  });
 
 module.exports = router;
