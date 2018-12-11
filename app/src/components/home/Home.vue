be423184e728160628e74e2dc4e72686ce66ed1d
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-10 17:12:27 -0800

    auth.vue is built out

---
+++ app/src/components/home/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <h2>Welcome to my goals!</h2>
+        <p>
+            <RouterLink to="/goals">View goals</RouterLink>
+        </p>
+    </section>
+</template>
+
+<style>
+
+</style>
