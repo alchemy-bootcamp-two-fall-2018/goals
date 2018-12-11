e33ce29e8d98927578db460a1d283cbf9c206ebf
Shaba9 <shaba92@gmail.com>
2018-12-11 09:13:09 -0800

    add form added and content console logs

--- app/src/components/Goals.vue
+++ app/src/components/Goals.vue
@@ -1,10 +1,34 @@
 <template>
-  <h2>goals page</h2>
+  <section>
+    <h2>Add a Goal:</h2>
+    <form @submit.prevent="onSubmit">
+      <label>Title:
+        <input v-model="goal.title">
+      </label>
+      <label>Start Date:
+        <input v-model="goal.stDate">
+      </label>
+      <label>End Date:
+        <input v-model="goal.enDate">
+      </label>
+      <button>Add</button>
+    </form>
+  </section>
+
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      goal: {}
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.goal);
+    }
+  }
 };
 </script>
 
