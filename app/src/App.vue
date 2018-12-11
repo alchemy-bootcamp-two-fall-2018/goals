8847c4e6eb2612cb7fc1712d6287fdfaf6677947
Abel Quintero <abel.j.quintero@gmail.com>
2018-12-10 16:39:54 -0800

    token set up in App

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,22 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <span v-if="user">
+        Hello {{user.username}}!
+      </span>
+      <nav v-if="user">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/goals">Goals</RouterLink>
+        <a href="#" @click="handleLogout">Logout</a>
+      </nav>
+    </header>
+
+    <main>
+      <RouterView v-if="user" :user="user"/>
+      <Auth v-else
+        :onSignUp="handleSignUp"
+        :onSignIn="handleSignIn"
+      />
+    </main>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import api from './services/api.js';
+import Auth from '../src/components/auth/Auth';
 
 export default {
-  name: 'app',
+  data() {
+    return {
+      user: null
+    };
+  },
   components: {
-    HelloWorld
+    Auth
+  },
+  created() {
+    const json = window.localStorage.getItem('profile');
+    if(json) {
+      this.setUser(JSON.parse(json));
+    }
+  },
+  methods: {
+    handleSignUp(profile) {
+      return api.signUp(profile)
+        .then(user => {
+          this.setUser(user);
+        });
+    },
+    handleSignIn(credentials) {
+      return api.signIn(credentials)
+        .then(user => {
+          this.setUser(user);
+        });
+    },
+    setUser(user) {
+      this.user = user;
+      if(user) {
+        api.setToken(user.id);
+        window.localStorage('profile', JSON.stringify(user));
+      }
+      else {
+        api.setToken();
+        window.localstorage.removeItem('profile');
+      }
+    },
+    handleLogout() {
+      this.setUser(null);
+      this.$router.push('/');
+    }
+
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +83,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+header {
+  height: 75px;
+  background: lightsteelblue;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+header img {
+  height: 100%;
+}
+nav a {
+  text-decoration: none;
+  color: black;
+  margin: 3px;
+  padding: 3px;
+  border: 1px solid black;
+}
+main {
+  padding: 8px;
 }
 </style>
