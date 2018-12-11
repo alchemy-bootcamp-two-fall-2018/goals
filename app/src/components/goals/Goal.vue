d922f988d44f6af4fd12b6c71876f7333fcfc6ad
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-10 17:21:38 -0800

    GoalList, Goal, and AddGoal are done

---
+++ app/src/components/goals/Goal.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <li>{{goal.name}} the {{goal.type}}</li>
+</template>
+
+<script>
+export default {
+  props: {
+    goal: null
+  }
+};
+</script>
+
+<style lang="postcss" scoped>
+li {
+  padding: 10px 0;
+}
+</style>
