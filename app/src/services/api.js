ebffb0c3fc446c863de20dd30a65d313b44c794e
Abel Quintero <abel.j.quintero@gmail.com>
2018-12-11 14:57:52 -0800

    fixed api to update goal

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,22 +1,5 @@
 let token = '';
 
-const getOptions = (method, data) => {
-  const options = {
-    method,
-    headers: {}
-  };
-
-  if(data) {
-    options.headers['Content-Type'] = 'application/json';
-    options.body = JSON.stringify(data);
-  }
-
-  if(token) {
-    options.headers.Authorization = token;
-  }
-
-  return options;
-};
 
 export default {
   
@@ -25,7 +8,13 @@ export default {
   },
 
   signUp(profile) {
-    return fetch('/api/auth/signup', getOptions('POST', profile))
+    return fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(profile)
+    })
       .then(response => {
         if(response.ok) {
           return response.json();
@@ -39,7 +28,13 @@ export default {
   },
 
   signIn(credentials) {
-    return fetch('/api/auth/signin', getOptions('POST', credentials))
+    return fetch('/api/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
       .then(response => {
         if(response.ok) {
           return response.json();
@@ -53,12 +48,37 @@ export default {
   },
 
   getGoals() {
-    return fetch('/api/goals', getOptions('GET'))
+    return fetch('/api/goals', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      }
+    })
       .then(response => response.json());      
   },
 
   addGoal(goal) {
-    return fetch('/api/goals', getOptions('POST', goal))
+    return fetch('/api/goals', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      },
+      body: JSON.stringify(goal)
+    })
+      .then(response => response.json());
+  },
+
+  updateGoal(goal) {
+    return fetch(`/api/goals/${goal.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      },
+      body: JSON.stringify(goal)
+    })
       .then(response => response.json());
   }
 
