d39730bc89fc61f828c0d31e54828404f40c8799
JeiWolfei <jvampy22@gmail.com>
2018-12-11 23:08:06 -0800

    aggregations added to goals.js

--- server/lib/routes/goals.js
+++ server/lib/routes/goals.js
@@ -20,37 +20,56 @@ router
     });
   })
     
-    .post('/', (req, res) => {
-      const body = req.body;
+  .post('/', (req, res) => {
+    const body = req.body;
+
+    client.query(`
+      INSERT INTO goal (name, start_date, end_date, profile_id)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `,
+    [body.name, body.startDate, body.endDate, req.userId])
+      .then(result => {
+        res.json(result.rows[0]);
+      });
+  })
   
-      client.query(`
-        INSERT INTO goal (name, start_date, end_date, profile_id)
-        VALUES ($1, $2, $3, $4)
-        RETURNING *;
-      `,
-      [body.name, body.startDate, body.endDate, req.userId])
-        .then(result => {
-          res.json(result.rows[0]);
-        });
-    })
-    
-    .put('/:id/completed', (req, res) => {
-      const completed = req.body.completed;
-  
-      client.query(`
-        UPDATE goal
-        SET completed = $1
-        WHERE id = $2
-        AND profile_id = $3
-        RETURNING *;
-      `,
-      [completed, req.params.id, req.userId]
-      )
-        .then(result => {
-          res.json(result.rows[0]);
-        });
+  .put('/:id/completed', (req, res) => {
+    const completed = req.body.completed;
+
+    client.query(`
+      UPDATE goal
+      SET 
+        name = $1
+        start_date = $2,
+        end_date = $3
+      WHERE id = $4
+      AND profile_id = $5
+      RETURNING *;
+    `,
+    [body.name, body.startDate, body.endDate, req.params.id, req.userId]
+    )
+      .then(result => {
+        res.json(result.rows[0]);
+      })
+});
+
+router.get('/stats', (req, res) => {
+  client.query(`
+  SELECT 
+    profile_id,
+    MIN(end_date - start_date) as mindiff,
+    MAX(end_date - start_date) as maxdiff,
+    CAST(AVG(end_date - start_date) as int) as average,
+    COUNT(goal.id) as count
+  FROM goal
+  GROUP BY profile_id;
+  `,
+  []
+  )
+    .then(result => {
+      res.json(result.rows);
     });
-    
-  
+});
 
 module.exports = router;
