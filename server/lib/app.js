42d38da5ceb20e28600f11a6f1d221f2da7572e1
‘Michael <pickup.patterns@gmail.com>
2018-12-11 17:13:44 -0800

    added checkAuth logic to app js

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const auth = require('./routes/auth');
+const goals = require('./routes/goals');
 const morgan = require('morgan');
-// const goals = require('./routes/goals');
-// const profiles = require('./routes/profiles');
+
 
 // enhanced logging using morgan
 app.use(morgan('dev'));
@@ -10,8 +11,17 @@ app.use(morgan('dev'));
 // register the json "middleware" body parserers
 app.use(express.json());
 
+function checkAuth(req, res, next) {
+  const userId = req.get('Authorization');
+  if(!userId) {
+    res.status(401).json({ error: 'no authorization found' });
+    return;
+  }
+  req.userId = userId;
+  next();
+}
 // register our routes
-// app.use('/api/profiles', profiles);
-// app.use('/api/goals', goals);
+app.use('/api/auth', auth);
+app.use('/api/goals', checkAuth, goals);
 
 module.exports = app;
