48d6f9d6d3e601a357dc2d33af5abb64d346adae
‘Michael <pickup.patterns@gmail.com>
2018-12-15 19:07:05 -0800

    added props for goal and mustache linkage for db collection

--- app/src/components/goals/Goal.vue
+++ app/src/components/goals/Goal.vue
@@ -1,11 +1,14 @@
 <template>
-    
+    <li>{{goal.name}}  {{goal.date}} {{goal.description}}</li>
 </template>
 
 <script>
 export default {
-
+  props: {
+    goal: null
+  }
 };
+
 </script>
 
 <style>
